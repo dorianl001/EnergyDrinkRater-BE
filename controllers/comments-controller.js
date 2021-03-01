75ff7011fcf7fc82c004107c63b17bd699fd7270
dorianl <dorian.lopez001@gmail.com>
2021-03-01 18:35:13 -0500

    added restful func to comment controller

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -7,4 +7,31 @@ router.get('/', (req, res) => {
     commRouter.find({}).then((comment) => res.json(comment));
 });
 
+//list a comment by id 
+router.get('/id:', (req, res) => {
+    const id = req.params.id;
+    commRouter.find({}).then((comment) => res.json(comment));
+});
+
+//create a new comment 
+router.get('/', (req, res => {
+    commRouter
+    .create(req.body)
+    .then((comment) => res.json(comment))
+    .catch(next);
+}));
+
+//update a single comment 
+router.put('/:id', (req, res) => {
+    commRouter
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(commRouter.find({}).then((comment) => res.json(comment)));
+});
+
+//delete a comment by id
+router.delete('/id:', (req, res) => {
+    const id = req.params.id;
+    commRouter.findOneAndDelete({ _id: id }).then((comment) => res.json(comment));
+})
+
 module.exports = router; 
