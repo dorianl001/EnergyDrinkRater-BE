c9d71167fd00482258e352aeefef51bcc61f83e7
dorianl <dorian.lopez001@gmail.com>
2021-03-09 16:34:15 -0500

    fixing some spacing issues

--- db/seeds.js
+++ db/seeds.js
@@ -1,6 +1,8 @@
 const energyDrink = require('../models/energydrink-model');
 const energyDrinks = require('../models/energydrink-model');
 const enDrinkSeeds = require('./seeds.json');
+
+
 energyDrinks.deleteMany({})
     .then(() => {
         return energyDrink.insertMany(enDrinkSeeds);
